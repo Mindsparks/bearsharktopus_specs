cb619c1e5455f56ba2ab123ba5b559d3494c99cd
Ben Slepp <blepp@MBP15r-1G8WP.local>
2016-09-27 15:45:17 -0400

    Addition of sliders, table, page layout, fixed header fonts, links.js, etc.

---
+++ js/links.js
@@ -0,0 +1,10 @@
+document.getElementById("links").innerHTML =
+'<div class="links">' +
+  '<a href="sidebar.html">SIDEBAR</a>' +
+  '<a href="header.html">HEADER</a>' +
+  '<a href="table.html">TABLE</a>' +
+  '<a href="colors.html">COLORS</a>' +
+  '<a href="fonts.html">FONTS</a>' +
+  '<a href="uikit.html">UI KIT</a>' +
+  '<a href="icons.html">ICONS</a>' +
+'</div>';
