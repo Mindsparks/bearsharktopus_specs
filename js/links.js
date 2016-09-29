691110925a752b00f6f2d533ef50f247ea53b51c
Ben Slepp <blepp@MBP15r-1G8WP.local>
2016-09-29 16:25:28 -0400

    Feature related classes added, beginning of transition to new index navigation.

--- js/links.js
+++ js/links.js
@@ -1,10 +1,11 @@
 document.getElementById("links").innerHTML =
 '<div class="links">' +
-  '<a href="sidebar.html">SIDEBAR</a>' +
-  '<a href="header.html">HEADER</a>' +
-  '<a href="table.html">TABLE</a>' +
+  '<a href="uikit.html">UI KIT</a>' +
+  '<a href="builder.html">BUILDER</a>' +
   '<a href="colors.html">COLORS</a>' +
   '<a href="fonts.html">FONTS</a>' +
-  '<a href="uikit.html">UI KIT</a>' +
   '<a href="icons.html">ICONS</a>' +
+  '<a href="table.html">TABLE</a>' +
+  '<a href="create-schedule.html">PAGE PREVIEW</a>' +
+  '<a href="sidebar-expanded.html">SIDEBAR EXPANDED</a>' +
 '</div>';
